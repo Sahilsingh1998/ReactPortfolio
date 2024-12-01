e1045657c9f9cfcb4448d848173f431c992e6bc7
Sahil Singh <singhsahil.8340@gmail.com>
2024-12-01 23:20:14 +0530

    sds

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Contact() {
+const ContactForm = () => {
   const [formData, setFormData] = useState({
     Name: "",
     Email: "",
@@ -18,7 +18,7 @@ function Contact() {
 
     try {
       const response = await fetch(
-        "https://script.google.com/macros/s/AKfycbxUDWN4V0IbaqEolVESkEtNRIpzcGCOHsmEU2pX8PPYsDhke-FC6VkjPPERaZoZqhLDww/exec",
+        "https://script.google.com/macros/s/AKfycbwbN2ChUGsa3p1Oh0opLBBCBp_Mj0AomUqH3GRf6y8zvHQasZGbPq-hoYByaJrYAv_xRQ/exec",
         {
           method: "POST",
           headers: {
@@ -42,122 +42,127 @@ function Contact() {
   };
 
   return (
-    <>
-      <div className="container contact" id="contact">
-        <h1>CONTACT US</h1>
-        <section className="contact_us mt-5">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-10 offset-md-1">
-                <div className="contact_inner">
-                  <div className="row">
-                    <div className="col-md-10">
-                      <div className="contact_form_inner">
-                        <div className="contact_field">
-                          <h3>Contact Us</h3>
-                          <p>
-                            Feel Free to contact us any time. We will get back
-                            to you as soon as we can!.
-                          </p>
-                          <form onSubmit={handleSubmit}>
-                            <input
-                              type="text"
-                              name="Name"
-                              className="form-control form-group py-2"
-                              placeholder="Name"
-                              value={formData.Name}
-                              onChange={handleChange}
-                              required
-                            />
-                            <input
-                              type="text"
-                              name="Email"
-                              className="form-control form-group py-2"
-                              placeholder="Email"
-                              value={formData.Email}
-                              onChange={handleChange}
-                              required
-                            />
-                            <input
-                              type="text"
-                              name="phone"
-                              className="form-control form-group py-2"
-                              placeholder="Phone no"
-                              value={formData.phone}
-                              onChange={handleChange}
-                              required
-                            />
-                            <textarea
-                              className="form-control form-group py-2"
-                              name="Message"
-                              placeholder="Message"
-                              value={formData.Message}
-                              onChange={handleChange}
-                              required
-                            ></textarea>
-                            <button
-                              type="submit"
-                              className="btn btn-primary contact_form_submit"
-                            >
-                              Send
-                            </button>
-                          </form>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-md-2">
-                      <div className="right_conatct_social_icon d-flex align-items-end">
-                        <div className="socil_item_inner d-flex">
-                          <li>
-                            <a
-                              href="https://www.facebook.com/share/q2cd6v3zzMU3bDs9/?mibextid=qi2Omg"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <i className="fab fa-facebook-square"></i>
-                            </a>
-                          </li>
-                          <li>
-                            <a
-                              href="https://www.instagram.com/it_sahil_rajput?igsh=MWNoM2ZhY3UwYWJhMw=="
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <i className="fab fa-instagram"></i>
-                            </a>
-                          </li>
-                          <li>
-                            <a
-                              href="https://x.com/sahilsingh7782?t=GYekHIEViuZyDbat-Ml4kw&s=08"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <i className="fab fa-twitter"></i>
-                            </a>
-                          </li>
-                        </div>
+    <div className="container contact" id="contact">
+      <h1>CONTACT US</h1>
+      <section className="contact_us mt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-10 offset-md-1">
+              <div className="contact_inner">
+                <div className="row">
+                  <div className="col-md-10">
+                    <div className="contact_form_inner">
+                      <div className="contact_field">
+                        <h3>Contact Us</h3>
+                        <p>
+                          Feel free to contact us anytime. We will get back to
+                          you as soon as we can!
+                        </p>
+                        <form onSubmit={handleSubmit}>
+                          <input
+                            type="text"
+                            name="Name"
+                            className="form-control form-group py-2"
+                            placeholder="Name"
+                            value={formData.Name}
+                            onChange={handleChange}
+                            required
+                          />
+                          <input
+                            type="email"
+                            name="Email"
+                            className="form-control form-group py-2"
+                            placeholder="Email"
+                            value={formData.Email}
+                            onChange={handleChange}
+                            required
+                          />
+                          <input
+                            type="tel"
+                            name="phone"
+                            className="form-control form-group py-2"
+                            placeholder="Phone no"
+                            value={formData.phone}
+                            onChange={handleChange}
+                            required
+                          />
+                          <textarea
+                            className="form-control form-group py-2"
+                            name="Message"
+                            placeholder="Message"
+                            value={formData.Message}
+                            onChange={handleChange}
+                            required
+                          />
+                          <button type="submit" className="contact_form_submit">
+                            Send
+                          </button>
+                        </form>
                       </div>
                     </div>
                   </div>
-                  <div className="contact_info_sec">
-                    <h4>Contact Info</h4>
-                    <div className="d-flex info_single align-items-center">
-                      <i className="fas fa-headset"></i>
-                      <span>+91 7782864311</span>
+                  <div className="col-md-2">
+                    <div className="right_conatct_social_icon d-flex align-items-end">
+                      <div className="socil_item_inner d-flex">
+                        <li>
+                          <a
+                            href="https://www.facebook.com/share/q2cd6v3zzMU3bDs9/?mibextid=qi2Omg"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <i className="fab fa-facebook-square"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.instagram.com/it_sahil_rajput?igsh=MWNoM2ZhY3UwYWJhMw=="
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <i className="fab fa-instagram"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://x.com/sahilsingh7782?t=GYekHIEViuZyDbat-Ml4kw&s=08"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <i className="fab fa-twitter"></i>
+                          </a>
+                        </li>
+                      </div>
                     </div>
-                    <div className="d-flex info_single align-items-center">
-                      <i className="fas fa-envelope-open-text"></i>
-                      <span>singhsahil.8340@gmail.com</span>
-                    </div>
+                  </div>
+                </div>
+                <div className="contact_info_sec">
+                  <h4>Contact Info</h4>
+                  <div className="d-flex info_single align-items-center">
+                    <i className="fas fa-headset"></i>
+                    <span>+91 7782864311</span>
+                  </div>
+                  <div className="d-flex info_single align-items-center">
+                    <i className="fas fa-envelope-open-text"></i>
+                    <span>singhsahil.8340@gmail.com</span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-      </div>
-    </>
-  );
-}
+        </div>
+      </section>
 
-export default Contact;
+      <div className="container-fluid mt-5">
+        <div className="responsive-map">
+          <iframe
+            title="Address"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7205.258830412314!2d87.24579554199615!3d25.45065026019435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f0182634c26067%3A0x26b88ea67dd3e779!2sKursela%2C%20Bihar!5e0!3m2!1sen!2sin!4v1710963292649!5m2!1sen!2sin"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
